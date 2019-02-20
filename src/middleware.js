e47ef3441f08f1780d1daf06d6ec805187439fb0
Ryan Morr <rm.morr@gmail.com>
2019-02-20 13:38:17 -0500

    Define initial implementation

---
+++ src/middleware.js
@@ -0,0 +1,51 @@
+class Middleware {
+    constructor(callback) {
+        this.callback = callback;
+        this.middleware = [];
+    }
+
+    use(callback) {
+        this.middleware.push(callback);
+    }
+
+    run(data) {
+        return new Promise((resolve, reject) => {
+            let resolved = false;
+            const error = (obj) => {
+                if (resolved) {
+                    return;
+                }
+                resolved = true;
+                reject(obj);
+            };
+            const done = (obj) => {
+                if (resolved) {
+                    return;
+                }
+                resolved = true;
+                resolve(obj);
+            };
+            const middleware = this.middleware;
+            if (middleware[0]) {
+                let i = 0;
+                const next = () => {
+                    if (!resolved) {
+                        const mw = middleware[++i];
+                        if (mw) {
+                            mw(data, next, error);
+                        } else {
+                            this.callback(data, done, error);
+                        }
+                    }
+                };
+                middleware[i](data, next, error);
+            } else {
+                this.callback(data, done, error);
+            }
+        });
+    }
+}
+
+export default function middleware(callback) {
+    return new Middleware(callback);
+}
--- test/specs/middleware.js
+++ test/specs/middleware.js
@@ -3,7 +3,21 @@ import { expect } from 'chai';
 import middleware from '../../src/middleware';
 
 describe('middleware', () => {
-    it('', () => {
-        
+    it('should invoke the callback, pass the data, and resolve the promise when run', (done) => {
+        const callback = sinon.spy((data, resolve, reject) => {
+            expect(data).to.equal('foo');
+            expect(reject).to.be.a('function');
+            expect(resolve).to.be.a('function');
+            resolve('bar');
+        });
+
+        const mw = middleware(callback);
+        const promise = mw.run('foo');
+
+        expect(promise).to.be.a('promise');
+        promise.then((data) => {
+            expect(data).to.equal('bar');
+            done();
+        });
     });
 });
