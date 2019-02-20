952611fe1cc089c93e42c29884c0f9ec3036b49c
Ryan Morr <rm.morr@gmail.com>
2019-02-20 13:18:21 -0500

    Initial commit

---
+++ test/specs/middleware.js
@@ -0,0 +1,9 @@
+import sinon from 'sinon';
+import { expect } from 'chai';
+import middleware from '../../src/middleware';
+
+describe('middleware', () => {
+    it('', () => {
+        
+    });
+});
