85e3fd5a55a2a3d9a24a61c54efa9cdd71ae29f6
Ryan Morr <rm.morr@gmail.com>
2019-02-20 21:07:09 -0500

    Refactor to use closure

--- src/middleware.js
+++ src/middleware.js
@@ -1,51 +1,45 @@
-class Middleware {
-    constructor(callback = (data, next) => next(data)) {
-        this.callback = callback;
-        this.middleware = [];
-    }
+export default function middleware(callback = (data, next) => next(data)) {
+    const middleware = [];
 
-    use(callback) {
-        this.middleware.push(callback);
-    }
+    return {
+        use(callback) {
+            middleware.push(callback);
+        },
 
-    run(data) {
-        return new Promise((resolve, reject) => {
-            let resolved = false;
-            const error = (obj) => {
-                if (resolved) {
-                    return;
-                }
-                resolved = true;
-                reject(obj);
-            };
-            const done = (obj) => {
-                if (resolved) {
-                    return;
-                }
-                resolved = true;
-                resolve(obj);
-            };
-            const middleware = this.middleware;
-            if (middleware[0]) {
-                let i = 0;
-                const next = () => {
-                    if (!resolved) {
-                        const mw = middleware[++i];
-                        if (mw) {
-                            mw(data, next, error);
-                        } else {
-                            this.callback(data, done, error);
-                        }
+        run(data) {
+            return new Promise((resolve, reject) => {
+                let resolved = false;
+                const error = (obj) => {
+                    if (resolved) {
+                        return;
                     }
+                    resolved = true;
+                    reject(obj);
                 };
-                middleware[i](data, next, error);
-            } else {
-                this.callback(data, done, error);
-            }
-        });
-    }
-}
-
-export default function middleware(callback) {
-    return new Middleware(callback);
+                const done = (obj) => {
+                    if (resolved) {
+                        return;
+                    }
+                    resolved = true;
+                    resolve(obj);
+                };
+                if (middleware[0]) {
+                    let i = 0;
+                    const next = () => {
+                        if (!resolved) {
+                            const mw = middleware[++i];
+                            if (mw) {
+                                mw(data, next, error);
+                            } else {
+                                callback(data, done, error);
+                            }
+                        }
+                    };
+                    middleware[i](data, next, error);
+                } else {
+                    callback(data, done, error);
+                }
+            });
+        }
+    };
 }
