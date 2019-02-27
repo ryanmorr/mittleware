b266c2d79fa8648a283987ff55e28f6eb3e4a55d
Ryan Morr <rm.morr@gmail.com>
2019-02-27 13:18:01 -0500

    Refactor

--- src/middleware.js
+++ src/middleware.js
@@ -8,36 +8,19 @@ export default function middleware() {
 
         dispatch(data) {
             return new Promise((resolve, reject) => {
-                let resolved = false;
-                const error = (obj) => {
-                    if (resolved) {
-                        return;
-                    }
-                    resolved = true;
-                    reject(obj);
-                };
-                const done = (obj) => {
-                    if (resolved) {
-                        return;
-                    }
-                    resolved = true;
-                    resolve(obj);
-                };
                 if (middleware[0]) {
                     let i = 0;
                     const next = () => {
-                        if (!resolved) {
-                            const mw = middleware[++i];
-                            if (mw) {
-                                mw(data, next, done, error);
-                            } else {
-                                done(data);
-                            }
+                        const mw = middleware[++i];
+                        if (mw) {
+                            mw(data, next, resolve, reject);
+                        } else {
+                            resolve(data);
                         }
                     };
-                    middleware[i](data, next, done, error);
+                    middleware[i](data, next, resolve, reject);
                 } else {
-                    done(data);
+                    resolve(data);
                 }
             });
         }
